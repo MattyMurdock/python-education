bb9d6a4285747690a0a95ecea59eab5609554ea7
MattyMurdock <oswaldlazarof.99@gmail.com>
2025-03-01 18:06:26 -0600

    [ADD] Sobre listas

---
+++ listas.py
@@ -0,0 +1,8 @@
+# Crear una lista de frutas
+frutas = ["manzana", "plátano", "naranja", "uva", "Mango"]
+
+# Imprimir la lista completa
+print("Lista de frutas:", frutas)
+
+# Imprimir el primer elemento de la lista
+print("Mi fruta favorita es:", frutas[4])
